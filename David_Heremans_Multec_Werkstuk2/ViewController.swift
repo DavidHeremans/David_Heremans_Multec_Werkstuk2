ffa864e8a336fd566d69175be7a26c613f51fee5
Astrid Heremans <astridheremans@macbook-pro-van-astrid.local>
2018-08-18 11:23:57 +0200

    connectie met json file proberen te maken

--- David_Heremans_Multec_Werkstuk2/ViewController.swift
+++ David_Heremans_Multec_Werkstuk2/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var myMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,5 +27,38 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
 
 
+}
+
+func getData(){
+    let url = URL(string: "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale")
+    let urlRequest = URLRequest(url: url!)
+    
+    
+    let session = URLSession(configuration: URLSessionConfiguration.default)
+    
+    let task = session.dataTask(with: urlRequest){
+        (data, response, error) in
+        guard error == nil else {
+            print("error calling GET")
+            print(error!)
+            return
+        }
+        guard let responseData = data else {
+            print("Error: did not receive data")
+            return
+        }
+        do {
+            guard let json = JSONSerialization.jsonObject(with: responseData, options: []) as? [String: AnyObject]
+        }
+        
+        for station in json! {
+            let naam = station["name"] as! String
+            let aantalBikeStands = station["bike_stands"] as! Int
+            let aantalBikeStandsAvailable = station["available_bike_stands"] as! Int
+            let aantalBikesAvailable = station["available_bikes"] as! Int
+        }
+    }
+    task.resume()
+    
 }
 
