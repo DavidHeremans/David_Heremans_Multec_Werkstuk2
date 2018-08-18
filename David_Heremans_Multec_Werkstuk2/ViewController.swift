d93a5ffaf9d6edf0d89c29fdcec9084b70bd343f
Astrid Heremans <astridheremans@mbp-van-astrid>
2018-08-18 10:12:09 +0200

    Initial Commit

---
+++ David_Heremans_Multec_Werkstuk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  David_Heremans_Multec_Werkstuk2
+//
+//  Created by Astrid Heremans on 18/08/18.
+//  Copyright © 2018 David Heremans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
